201f99513528715e7effddbd2e2f878a5aa5ea44
TomekWebDev <tommaso.mangiagalli@gmail.com>
2023-01-13 16:08:15 +0100

    done

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -1,5 +1,108 @@
 @extends('layouts.app')
 
 @section('main')
-    <div class="main2"> porco dio</div>
+    <div class="blue-under-header bg-primary">
+    </div>
+
+    <div class="comic-container-positioned">
+        <img src="{{ $single_comic['thumb'] }}" alt="">
+        <div class="comic-book">
+            COMIC BOOK
+        </div>
+
+        <div class="view-gallery">
+            VIEW GALLERY
+        </div>
+    </div>
+
+    <div class="show-card w-100 m-auto text-center py-5">
+
+        <h2 class="">{{ $single_comic['title'] }}</h2>
+
+        <div class="green-box p-2 m-auto bg-success text-white d-flex justify-content-around w-50">
+
+            <div class="left-green d-flex justify-content-between w-75">
+                <div>U.S. Price:{{ $single_comic['price'] }}</div>
+                <div>AVAILABLE</div>
+            </div>
+
+            <div class="check-availability w-25">
+                Check Availability
+            </div>
+
+        </div>
+
+        <div class="p-2 m-auto d-flex justify-content-around w-50">
+            <p>
+                {{ $single_comic['description'] }}
+            </p>
+        </div>
+
+    </div>
+
+
+    <div class="w-100 bg-light">
+
+        <div class="w-75 py-5 m-auto d-flex ">
+
+            <div class="talent w-50">
+
+                <h4>Talent</h4>
+
+                <div class="separee w-100 bg-dark"></div>
+                <div class="d-flex py-2">
+                    <div class="w-25">Art by:</div>
+                    <div class="w-75 text-primary">
+                        @foreach ($single_comic['artists'] as $artist)
+                            {{ $artist }},
+                        @endforeach
+                    </div>
+                </div>
+                <div class="separee w-100 bg-dark"></div>
+
+                <div class="d-flex py-2">
+                    <div class="w-25">Written by:</div>
+                    <div class="w-75 text-primary">
+                        @foreach ($single_comic['writers'] as $artist)
+                            {{ $artist }},
+                        @endforeach
+                    </div>
+                </div>
+                <div class="separee w-100 bg-dark"></div>
+            </div>
+
+            <div class="specs w-25 mx-5">
+
+                <h4>Specs</h4>
+
+                <div class="separee w-100 bg-dark"></div>
+                <div class="d-flex py-2">
+                    <div class="w-25">Series:</div>
+                    <div class="w-75 text-primary">
+                        {{ $single_comic['series'] }}
+                    </div>
+                </div>
+                <div class="separee w-100 bg-dark"></div>
+
+                <div class="d-flex py-2">
+                    <div class="w-25">U.S. Price:</div>
+                    <div class="w-75">
+                        {{ $single_comic['price'] }}
+                    </div>
+                </div>
+                <div class="separee w-100 bg-dark"></div>
+
+                <div class="d-flex py-2">
+                    <div class="w-25">On Sale Date:</div>
+                    <div class="w-75">
+                        {{ $single_comic['sale_date'] }}
+                    </div>
+                </div>
+                <div class="separee w-100 bg-dark"></div>
+
+            </div>
+
+        </div>
+
+    </div>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,14 +7,25 @@
 
             <div class=" d-flex flex-wrap">
 
-                @foreach ($comics as $elem)
+                @foreach ($comics as $key => $elem)
                     <div class="card-container">
                         <div class="card-image">
+
                             <img class="card-img" class="comic-image" src="{{ $elem['thumb'] }}" alt="Card image cap">
+
                         </div>
 
                         <div class="card-body">
-                            <h5 class="card-title fs-6 text-white">{{ $elem['title'] }}</h5>
+                            <h5 class="card-title fs-6 text-white">
+
+                                <a href="{{ route('comic', compact('key')) }}">
+
+                                    {{ $elem['title'] }}
+
+                                </a>
+
+
+                            </h5>
                         </div>
                     </div>
                 @endforeach
